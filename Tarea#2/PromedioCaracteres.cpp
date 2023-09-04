bffcffe0e0f5b5210c8624a6a1abe88ce0af7fc7
César Silva <127228753+NotMacaco22@users.noreply.github.com>
2023-09-03 22:38:38 -0600

    Update - Comments

--- Tarea#2/PromedioCaracteres.cpp
+++ Tarea#2/PromedioCaracteres.cpp
@@ -21,21 +21,21 @@ int main(){
         getline(cin,line);
         numLine++;
 
-        if (line.empty()){
+        if (line.empty()){ //Identifica cuando el usuraio ingresa una linea vacia para cambiar la variable booleana continueProgram a false y asi detener el ciclo
             continueProgram = false;
 
-            if (readLines == 0){
+            if (readLines == 0){ //Si no se ingresa ninguna linea se mostrara el siguiente mensaje en pantalla
                 cout << "Usted no ha ingresado ninguna linea." << endl;
             }
 
             else{
-                system("cls");
+                system("cls"); 
                 promedio = TotalCaracteres / readLines;
                 cout << "El promedio de caracteres de las lineas que ingreso es igual a: " << promedio << "." << endl;
             }    
         }
-        TotalCaracteres += line.length();
-        readLines++;
+        TotalCaracteres += line.length(); //Acumulador que todo el contenido de un enunciado para posteriormente ocuparlo para el promedio
+        readLines++; //Luego de ingresar una linea exitosamente se suamara el acumulador readLines que leera las lineas leidas que ayudara para el promedio
     }
     
     return 0;
