c3e2528ec56ef5bdd8ed4f2aafbb50e53653f8f0
César Silva <127228753+NotMacaco22@users.noreply.github.com>
2023-09-03 22:32:21 -0600

    Update - Comments

--- Tarea#2/8.9.cpp
+++ Tarea#2/8.9.cpp
@@ -21,13 +21,18 @@ int main(){
 
     cout << endl;
 
-    result = strcmp(firstSentence, secondSentence);
+    result = strcmp(firstSentence, secondSentence); //Compara ambos enunciados
+    /*El strcmp funciona de la siguiente manera, compara ambos enunciados y da un resultado que puede ser -1, 0 o 1
+    0 = Ambos enunciados son iguales
+    1 = Primera variable posee mayor cantidad de caracteres
+    -1 = Segunda varaible posee mayour cantidad de caracteres
+    */
 
     if (result == 0){
         cout << "Ambos tienen la misma cantidad de caracteres.\n";
     } else if (result == 1){
         cout << "El primer enunciado: " << firstSentence << " tienen la mayor cantidad de caracteres\n";
-    } else{
+    } else{ //En este else significaria que el valor arrojado por el strcmp es igual a -1, significando asi que la segunda variable (el segundo enunciado) es el que posee mayor caracteres
         cout << "El segundo enunciado: " << secondSentence << " tienen la mayor cantidad de caracteres\n";
     }
     
